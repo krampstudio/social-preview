9bba76e2f5058b8f0b55ad858c96778d4c0902af
Bertrand CHEVRIER <bertrand@huggingface.co>
2026-04-13 09:48:10 +0200

    show url

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-
+document.addEventListener('DOMContentLoaded', function () {
     // Get current tab URL and find og:image
     chrome.tabs.query({active: true, currentWindow: true}, tabs => {
         if (!tabs[0]) return;
@@ -14,14 +14,16 @@
             const imageUrl = new URL(results[0].result, tabs[0].url).toString();
             const previewDiv = document.getElementById('image-preview');
             const noImageDiv = document.getElementById('no-image');
+            const urlDiv = document.getElementById('url');
             // Create preview image
             const img = document.createElement('img');
             img.src = imageUrl;
             img.alt = "Social Thumbnail Preview";
             previewDiv.appendChild(img);
             noImageDiv.style.display = 'none';
+            urlDiv.innerHTML = `<a href="${imageUrl}" target="_blank">${imageUrl}</a>`;
             previewDiv.style.display = 'block';
         });
     });
-
+});
 
